cefd00842ee0ba6644172c5080d24d0bd97b9f51
Zsolt <divekizs@yahoo.com>
2019-12-05 23:31:14 +0100

    initialization of bet.hu download started

--- finance_portfolio.py
+++ finance_portfolio.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import os
 import datetime as dt
+from selenium import webdriver
+import requests
+from selenium.webdriver.support.ui import Select
+
 
 def last_not_weekend(tmp):
     while tmp.weekday() > 4:
@@ -30,8 +34,50 @@ def align_dates(df, to='D', start=None, end=None):
             start = df[df['Ticker']==tck].index.min()
         new_index = pd.date_range(start=start, end=end, freq=to)
         tmp = pd.concat([tmp, df[df['Ticker']==tck].reindex(new_index, method='ffill')])
-    return tmp 
+    return tmp
 
+
+class InitializeDownload:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def initialize_start_date(self):
+        pass
+
+    def initialize_end_date(self):
+        pass
+
+    def initialize_instrument(self):
+        pass
+
+    def initialize_time_range(self):
+        pass
+
+    def initialize_output_format(self):
+        pass
+
+    def initialize_output_type(self):
+        pass
+
+
+class InitializeDownload_BET(InitializeDownload):
+    def initialize_start_date(self):
+        pass
+
+    def initialize_end_date(self):
+        pass
+
+    def initialize_instrument(self):
+        pass
+
+    def initialize_time_range(self):
+        pass
+
+    def initialize_output_format(self):
+        pass
+
+    def initialize_output_type(self):
+        pass
 
 class DataBase:
     def __init__(self):
@@ -43,7 +89,7 @@ class DataBase:
 
     def _load_thickers(self):
         pass
-    
+
     def list_tickers(self):
         pass
 
@@ -52,8 +98,24 @@ class DataBase:
         newest_dates = [yesterday()] * last_dates.shape[0]
         down_input = list(zip(last_dates.index, last_dates.Date, newest_dates))
 
-    def download(self):
-        pass
+    def download(self, instrument, start, end):
+        self._start_webdriver()
+        url = self.tickers_db[self.tickers_db.Ticker == instrument].URL.values[0]
+        self.driver.get(url)
+        self._initialize_input(url)
+        # element=Select(self.driver.find_element_by_id('exchange-type'))
+        # element.select_by_value('shares')
+        # self.get_available_instruments()
+
+    def _initialize_input(self, url):
+        if 'bet.hu' in url:
+            pass
+
+    def _start_webdriver(self):
+        chrome_options = webdriver.ChromeOptions()
+        # chrome_options.add_argument('--headless')
+        # chrome_options.add_argument('--no-sandbox') # required when running as root user. otherwise you would get no sandbox errors.
+        self.driver = webdriver.Chrome(options=chrome_options)
 
     def load_history(self, tickers=[]):
         pass
@@ -63,9 +125,9 @@ class DataBase:
 
 class DataBaseExcel(DataBase):
     _path = './Instruments'
-    _tickers_name = 'tickers.xlsx' 
+    _tickers_name = 'tickers.xlsx'
     _history_name = 'data.xlsx'
-    
+
     def _load_thickers(self):
         path = os.path.join(self._path, self._tickers_name)
         self.tickers_db = pd.read_excel(path)
@@ -95,7 +157,7 @@ class Price:
     def load_prices(self, path):
         self.prices = pd.read_excel(path, index_col=0)
         self.prices.index = pd.to_datetime(self.prices.index)
-    
+
 
 class Ticker:
     path = './Instruments'
@@ -104,23 +166,23 @@ class Ticker:
 
     def __init__(self):
         self.load_tickers()
-        
+
     def load_tickers(self):
         self.ticker = pd.read_excel(os.path.join(self.path, self.filename))
-    
+
     def save_tickers(self, name=None):
         fn = name or self.filename
         fname = os.path.join(self.path, fn)
         self.ticker.to_excel(fname)
         print('Tickers are saved!')
-    
+
     def get_ticker(self, name):
         try:
             nm = self.ticker[self.ticker['Name']==name].Ticker
             return nm.values[0]
         except IndexError:
             print('`{a}` is not a valid identifier. Choose correctly. Try Ticker.ticker!'.format(a=name))
-    
+
     def get_name(self, ticker):
         try:
             tckr = self.ticker[self.ticker['Ticker']==ticker].Name
@@ -134,7 +196,7 @@ class Ticker:
             return ccy
         except IndexError:
             print('`{a}` or `{b}` is not a valid identifier. Choose correctly. Try Ticker.ticker!'.format(a=ticker, b=name))
-    
+
     def get_instrument_info(self, ticker=None, name=None, ccy=None):
         ccy = self.ticker[(self.ticker['Ticker']==ticker) | (self.ticker['Name']==name) | (self.ticker['Currency']==ccy)]
         return ccy
@@ -144,7 +206,7 @@ class Ticker:
             print('The input shoudl contain the following keys: {}'.format(self.header))
         else:
             self.ticker = self.ticker.append(td, ignore_index=True)
-    
+
     def remove_ticker(self, name=None, ticker=None):
         self.ticker = self.ticker[(self.ticker['Name'] != name) & (self.ticker['Ticker'] != ticker)]
 
@@ -155,17 +217,17 @@ class Trades:
     def __init__(self, filename=None):
         fn = self._check_file_path(filename)
         self.load_trades()
-    
+
     def load_trades(self, filename=None):
         fn = self._check_file_path(filename)
         self.instrument = pd.read_excel(fn)
         self.instrument['Date'] = pd.to_datetime(self.instrument['Date'])
         self.instrument = self.instrument.set_index('Date')
-    
+
     def save_trades(self, filename=None):
         fn = self._check_file_path(filename)
         self.instrument.to_excel(fn)
-    
+
     def _check_file_path(self, filename):
         fn = filename or os.path.join(self.pathname, self.filename)
         return fn
@@ -175,7 +237,7 @@ class Trades:
             self.instrument = self.instrument.append(other.instrument, ignore_index=True, sort=False)
         except AttributeError:
             print('Try to use an `Instrument` object as Input!')
-    
+
     def __str__(self):
         return(self.instrument.to_string())
 
@@ -192,10 +254,10 @@ class Instrument:
         self.currency = tickerdb.get_currency(ticker=dct.get('Ticker')[0])
         dct['Currency'] = self.currency
         self._construct_df(dct)
-    
+
     def _construct_df(self, dct):
         self.instrument = pd.DataFrame(dct)
-    
+
     def __str__(self):
         return('{self.ticker} '.format(self=self))
 
@@ -209,15 +271,15 @@ class Position:
             self.position=position
         else:
             self.calculate_positions()
-    
+
     def calculate_positions(self):
         self.position = self._trades.instrument[['Ticker', 'Account', 'Amount']].copy()
         # self.position.loc[:,'Position'] = 1
         self.position['Position'] = self.position.groupby(['Ticker', 'Account']).cumsum()
         self.position=self.position.drop(columns=['Amount'])
         self.position = align_dates(self.position)
-        
-        
+
+
 
 class Portfolio:
     def __init__(self):
